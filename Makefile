c1793e50e63c24bdf850022687e374afbfc21b1d
Natalia B <naffa.bob@gmail.com>
2024-11-16 20:18:50 +0300

    Добавить структуру проекта

---
+++ Makefile
@@ -0,0 +1,29 @@
+.PHONY: build
+build:
+	docker compose build
+
+.PHONY: start
+start:
+	docker compose up -d --build
+
+.PHONY: stop
+stop:
+	docker compose down
+
+.PHONY: format
+format:
+	black . && isort .
+
+.PHONY: makemigrations
+makemigrations:
+	@read -p "Enter migration message: " MSG; \
+	docker exec billing_api alembic revision --autogenerate -m "$$MSG"
+
+.PHONY: migrate
+migrate:
+	docker exec billing_api alembic upgrade head
+
+.PHONY: downgrade
+downgrade:
+	@read -p "Enter revision number: " MSG; \
+	 docker exec billing_api alembic downgrade "$$MSG"
---
+++ billing_api/src/main.py
@@ -0,0 +1,43 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from api.v1 import admin, billing
+from core.config import settings
+from db import postgres
+from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
+from fastapi_pagination import add_pagination
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    postgres.engine = create_async_engine(
+        postgres.dsn, echo=settings.engine_echo, future=True
+    )
+    postgres.async_session = async_sessionmaker(
+        bind=postgres.engine, expire_on_commit=False, class_=AsyncSession
+    )
+    yield
+
+
+app = FastAPI(
+    lifespan=lifespan,
+    title=settings.project_name,
+    docs_url="/api/openapi/",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+)
+
+app.include_router(
+    billing.router, prefix="/api/v1/billing", tags=["billing"]
+)
+app.include_router(
+    admin.router, prefix="/api/v1/admin/billing", tags=["admin_billing"]
+)
+
+add_pagination(app)
+
+# Для локального запуска
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8080, reload=True)
