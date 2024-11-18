537a422d27da6e66c757fc8c33db35696422e779
Natalia Bobkova <112754974+naffabob@users.noreply.github.com>
2024-11-18 14:30:48 +0300

    Добавить структуру проекта (#5)

---
+++ billing_api/src/db/postgres.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
+from sqlalchemy.orm import declarative_base
+
+from core.config import settings
+
+Base = declarative_base()
+
+engine: AsyncEngine | None = None
+async_session: AsyncSession | None = None
+
+dsn = settings.postgres_url
+
+
+async def get_postgres_session() -> AsyncSession | None:
+    return async_session
