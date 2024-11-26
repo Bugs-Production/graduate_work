dc9b2499eca84ad1c431e2a2d0ac2dd82d0d050d
vtitoff <v.titoff2010@gmail.com>
2024-11-26 13:26:47 +0300

    Добавить метод создания платежного намерения

--- billing_api/src/services/payment_process.py
+++ billing_api/src/services/payment_process.py
@@ -1,9 +1,12 @@
+import logging
 from abc import ABC, abstractmethod
 from uuid import UUID
 
 import stripe
 
 from core.config import settings
+
+logger = logging.getLogger(__name__)
 
 
 class BasePaymentProcessor(ABC):
@@ -12,6 +15,11 @@ class BasePaymentProcessor(ABC):
     @abstractmethod
     async def create_card(self, customer_id: str, card_id: UUID) -> str:
         """Создает карту юзера."""
+        pass
+
+    @abstractmethod
+    async def process_payment(self, amount: int, currency: str, customer_id: str, payment_id: str) -> dict:
+        """Инициализирует оплату"""
         pass
 
 
@@ -34,3 +42,20 @@ class PaymentProcessorStripe(BasePaymentProcessor):
         """Создание клиента на стороне Stripe."""
         customer = await stripe.Customer.create_async()  # type: ignore[attr-defined]
         return customer
+
+    async def process_payment(
+        self, amount: int, currency: str, customer_id: str, payment_id: str
+    ) -> dict:
+        """Создание платежного намерения"""
+        try:
+            payment_intent = stripe.PaymentIntent.create(
+                amount=amount,
+                currency=currency,
+                customer=customer_id,
+                confirm=True,
+                payment_method=payment_id,
+                off_session=True,
+            )
+            return {"client_secret": payment_intent["client_secret"]}
+        except stripe.error.StripeError as e:
+            logger.warning(f"Stripe error: {e}")
