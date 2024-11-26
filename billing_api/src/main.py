a3eb0a04156ea7841fd76ee6d40e3d070044be16
Ivan Skvortsov <pprofcheg@gmail.com>
2024-11-26 12:11:22 +0300

    добавлены роуты для планов подписки

--- billing_api/src/main.py
+++ billing_api/src/main.py
@@ -7,7 +7,7 @@ from fastapi_pagination import add_pagination
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from starlette.staticfiles import StaticFiles
 
-from api.v1 import admin, billing
+from api.v1 import admin, billing, subscription_plan
 from core.config import settings
 from db import postgres
 
@@ -15,7 +15,9 @@ from db import postgres
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     postgres.engine = create_async_engine(postgres.dsn, echo=settings.engine_echo, future=True)
-    postgres.async_session = async_sessionmaker(bind=postgres.engine, expire_on_commit=False, class_=AsyncSession)  # type: ignore[assignment]
+    postgres.async_session = async_sessionmaker(
+        bind=postgres.engine, expire_on_commit=False, class_=AsyncSession
+    )  # type: ignore[assignment]
     yield
 
 
@@ -29,6 +31,9 @@ app = FastAPI(
 
 app.include_router(billing.router, prefix="/api/v1/billing", tags=["billing"])
 app.include_router(admin.router, prefix="/api/v1/admin/billing", tags=["admin_billing"])
+app.include_router(
+    subscription_plan.router, prefix="/api/v1/subscription_plans", tags=["subscription_plans"]
+)
 
 add_pagination(app)
 app.mount("/static", StaticFiles(directory="static"), name="static")
