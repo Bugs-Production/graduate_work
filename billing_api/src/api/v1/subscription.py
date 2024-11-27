fdb9a8575480c9644f23c1c0ba9441373d18c0a0
Ivan Skvortsov <pprofcheg@gmail.com>
2024-11-27 15:51:37 +0300

    Добавлены эндпойнты и логика для обновления подписки и переключения режмиа авто-продления

--- billing_api/src/api/v1/subscription.py
+++ billing_api/src/api/v1/subscription.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, Path
 
 from api.utils import generate_error_responses
-from schemas.subscription import SubscriptionCreate, SubscriptionResponse
+from schemas.subscription import SubscriptionCreate, SubscriptionRenew, SubscriptionResponse
 from services.subscription_manager import SubscriptionManager, get_subscription_manager
 
 router = APIRouter()
@@ -28,8 +28,8 @@ async def create_subscription(
 @router.post(
     "/{subscription_id}/cancel",
     response_model=SubscriptionResponse,
-    summary="Создать подписку",
-    description="Создаёт подписку для пользователя",
+    summary="Отменить подписку",
+    description="Отменяет выбранную подписку пользователя",
     status_code=HTTPStatus.OK,
     responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
 )
@@ -39,3 +39,38 @@ async def cancel_subscription(
 ):
     user_id = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa8")  # TODO: забрать из токена
     return await subscription_manager.cancel_subscription(user_id=user_id, subscription_id=subscription_id)
+
+
+@router.post(
+    "/{subscription_id}/renew",
+    response_model=SubscriptionResponse,
+    summary="Обновить подписку",
+    description="Обновляет подписку, путем смещения даты завершения подписки",
+    status_code=HTTPStatus.OK,
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
+)
+async def renew_subscription(
+    renew_data: SubscriptionRenew,
+    subscription_id: UUID = Path(..., description="ID подписки"),
+    subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
+):
+    user_id = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa8")  # TODO: забрать из токена
+    return await subscription_manager.renew_subscription(
+        user_id=user_id, subscription_id=subscription_id, renew_data=renew_data
+    )
+
+
+@router.post(
+    "/{subscription_id}/toggle_auto_renewal",
+    response_model=SubscriptionResponse,
+    summary="Переключить режим автоматического продления подписки",
+    description="Включает режим автоматического продления подписки, если он был отключен и наоборот",
+    status_code=HTTPStatus.OK,
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
+)
+async def toggle_auto_renewal_subscription(
+    subscription_id: UUID = Path(..., description="ID подписки"),
+    subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
+):
+    user_id = UUID("3fa85f64-5717-4562-b3fc-2c963f66afa8")  # TODO: забрать из токена
+    return await subscription_manager.toggle_auto_renewal(user_id=user_id, subscription_id=subscription_id)
--- billing_api/src/services/subscription_manager.py
+++ billing_api/src/services/subscription_manager.py
@@ -9,47 +9,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from db.postgres import get_session
 from models.enums import SubscriptionStatus
 from models.models import Subscription
-from schemas.subscription import SubscriptionCreate, SubscriptionCreateFull, SubscriptionUpdate
+from schemas.subscription import SubscriptionCreate, SubscriptionCreateFull, SubscriptionRenew, SubscriptionUpdate
 from services.base import SQLAlchemyRepository
-from services.exceptions import (
-    AccessDeniedError,
-    ActiveSubscriptionExsistsError,
-    ObjectNotFoundError,
-    SubscriptionCancelError,
-)
+from services.exceptions import AccessDeniedError, ActiveSubscriptionExsistsError, SubscriptionCancelError
 from services.subscription_plan import SubscriptionPlanService
 
 
 class SubscriptionManager(SQLAlchemyRepository[Subscription, SubscriptionCreateFull, SubscriptionUpdate]):
+    # TODO: создание запроса на оплату подписки (payment intent)
+    # TODO: обработка ответа от платёжной системы (вебхука)
+    # TODO: изменение роли пользователя
+    # TODO: нотификации пользователю
     def __init__(self, session: AsyncSession, subscription_plan_service: SubscriptionPlanService):
         super().__init__(model=Subscription, session=session)
         self._subscription_plan_service = subscription_plan_service
 
-    async def user_has_active_subscription(self, user_id: UUID) -> bool:
-        """Проверяет наличие активной подписки у пользователя.
-
-        Считаем, что подписка является активной в том случае, если подписка оплачена и активна (active),
-        либо находится в режиме ожидания оплаты (pending).
-
-        Args:
-            user_id: id пользователя, для которого проверяется наличие подписки
-
-        Returns:
-            bool: True, если у пользователя есть подписка, False в противном случае
-        """
-        active_statuses = [SubscriptionStatus.ACTIVE, SubscriptionStatus.PENDING]
-        stmt = select(self._model).where(self._model.user_id == user_id, self._model.status.in_(active_statuses))
-        result = await self._session.execute(stmt)
-        return result.scalar_one_or_none() is not None
-
     async def create_subscription(self, user_id: UUID, subscription_data: SubscriptionCreate) -> Subscription:
         """Создаёт новую подписку для пользователя."""
-        if await self.user_has_active_subscription(user_id):
+        if await self._user_has_active_subscription(user_id):
             raise ActiveSubscriptionExsistsError(f"Пользователь с id={user_id} уже имеет подписку.")
 
-        subscription_plan = await self._subscription_plan_service.get_one_or_none(subscription_data.plan_id)
-        if not subscription_plan:
-            raise ObjectNotFoundError(f"План подписки с id={subscription_data.plan_id} не найден.")
+        subscription_plan = await self._subscription_plan_service.get(subscription_data.plan_id)
 
         start_date = datetime.now()
         end_date = start_date + timedelta(days=subscription_plan.duration_days)
@@ -64,38 +44,62 @@ class SubscriptionManager(SQLAlchemyRepository[Subscription, SubscriptionCreateF
         )
         return await self.create(full_subscription_data)
 
-    async def create_payment_intent_for_subscription(self, subscription_id, *args, **kwargs):
-        # TODO: инициировать платёж по подписке
-        pass
-
-    async def handle_payment_webhook_event(self, *args, **kwargs):
-        # TODO: обработка ответа от платёжной системы
-        pass
-
-    async def upgrade_user_to_subscriber(self):
-        pass
-
-    async def notify_user_role_changed(self):
-        pass
-
     async def cancel_subscription(self, user_id: UUID, subscription_id: UUID) -> Subscription:
         """Отменяет подписку пользователя."""
-        # TODO: проверка, возможно ли отменить подписку (по времени)
-        # TODO: возврат средств
-        subscription = await self.get_one_or_none(subscription_id)
-        if not subscription:
-            raise ObjectNotFoundError(f"Подписка с id={subscription_id} не найдена")
+        # TODO: проверка, возможно ли отменить подписку (можно отменить в течение N дней со старта?)
+        # TODO: возврат средств (платёжка)
+        subscription = await self._validate_subscription_access(user_id, subscription_id)
 
         if subscription.status not in [SubscriptionStatus.ACTIVE, SubscriptionStatus.PENDING]:
             raise SubscriptionCancelError("Ошибка при попытке отмены подписки. Проверьте статус подписки.")
-
-        if subscription.user_id != user_id:
-            raise AccessDeniedError("У вас недостаточно прав для совершения этого действия")
 
         update_data = SubscriptionUpdate(
             status=SubscriptionStatus.CANCELLED, auto_renewal=False, end_date=datetime.now()
         )
         return await self.update(subscription_id, update_data)
+
+    async def toggle_auto_renewal(self, user_id: UUID, subscription_id: UUID) -> Subscription:
+        """Переключает режим автоматического продления подписки.
+
+        Если автоматическое продление включено - отключает его и наоборот.
+        """
+        subscription = await self._validate_subscription_access(user_id, subscription_id)
+        new_auto_renewal = not subscription.auto_renewal
+        update_data = SubscriptionUpdate(auto_renewal=new_auto_renewal)
+        return await self.update(subscription.id, update_data)
+
+    async def renew_subscription(
+        self, user_id: UUID, subscription_id: UUID, renew_data: SubscriptionRenew
+    ) -> Subscription:
+        """Продляет подписку.
+
+        Под продлением понимается смещение даты завершения подписки на количество дней, указанных в плане подписки.
+        """
+        # TODO: обработка платежа для продления подписки.
+        subscription_plan = await self._subscription_plan_service.get(renew_data.plan_id)
+        subscription = await self._validate_subscription_access(user_id, subscription_id)
+        new_end_date = subscription.end_date + timedelta(days=subscription_plan.duration_days)
+        update_data = SubscriptionUpdate(end_date=new_end_date)
+        return await self.update(subscription.id, update_data)
+
+    async def _user_has_active_subscription(self, user_id: UUID) -> bool:
+        """Проверяет наличие активной подписки у пользователя.
+
+        Считаем, что подписка является активной в том случае, если подписка оплачена и активна (active),
+        либо находится в режиме ожидания оплаты (pending).
+        """
+        active_statuses = [SubscriptionStatus.ACTIVE, SubscriptionStatus.PENDING]
+        stmt = select(self._model).where(self._model.user_id == user_id, self._model.status.in_(active_statuses))
+        result = await self._session.execute(stmt)
+        return result.scalar_one_or_none() is not None
+
+    async def _validate_subscription_access(self, user_id: UUID, subscription_id: UUID) -> Subscription:
+        """Проверяет, что подписка с указанным subscription_id существует и редактирование подписки доступно
+        пользователю с указанным user_id."""
+        subscription = await self.get(subscription_id)
+        if subscription.user_id != user_id:
+            raise AccessDeniedError("У вас недостаточно прав для совершения этого действия")
+        return subscription
 
 
 @lru_cache
