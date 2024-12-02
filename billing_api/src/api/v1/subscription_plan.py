53160f8efd9c2a0681bdd939911f2660d02bd409
Natalia Bobkova <112754974+naffabob@users.noreply.github.com>
2024-12-02 19:16:38 +0500

    Админка: SubscriptionPlan (#59)

--- billing_api/src/api/v1/subscription_plan.py
+++ billing_api/src/api/v1/subscription_plan.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, Path
 from fastapi_pagination import Page, paginate
 
-from api.jwt_access_token import require_admin
+from api.jwt_access_token import AccessTokenPayload, UserRole, require_admin, security_jwt
 from api.utils import generate_error_responses
 from schemas.subscription_plan import SubscriptionPlanCreate, SubscriptionPlanResponse, SubscriptionPlanUpdate
 from services.subscription_plan import SubscriptionPlanService, get_subscription_plan_service
@@ -18,12 +18,16 @@ router = APIRouter()
     summary="Вывести планы подписок",
     description="Вывести все существующие планы подписок с пагинацией",
     status_code=HTTPStatus.OK,
-    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR),  # type: ignore[reportArgumentType]
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED),  # type: ignore[reportArgumentType]
 )
 async def get_subscription_plans(
     subscription_plan_service: SubscriptionPlanService = Depends(get_subscription_plan_service),
+    token: AccessTokenPayload = Depends(security_jwt),
 ):
-    subscription_plans = await subscription_plan_service.get_many()
+    if token.role == UserRole.ADMIN:
+        subscription_plans = await subscription_plan_service.get_many()
+    else:
+        subscription_plans = await subscription_plan_service.get_many({"is_archive": False})
     return paginate(subscription_plans)
 
 
@@ -33,7 +37,7 @@ async def get_subscription_plans(
     summary="Создать план подписки",
     description="Создание плана подписки",
     status_code=HTTPStatus.CREATED,
-    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR),  # type: ignore[reportArgumentType]
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED),  # type: ignore[reportArgumentType]
     dependencies=[Depends(require_admin)],
 )
 async def create_subscription_plan(
@@ -64,7 +68,9 @@ async def get_subscription_plan_by_id(
     summary="Обновить план подписки",
     description="Обновление данных выбранного плана подписки",
     status_code=HTTPStatus.OK,
-    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
+    responses=generate_error_responses(
+        HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.NOT_FOUND, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED
+    ),  # type: ignore[reportArgumentType]
     dependencies=[Depends(require_admin)],
 )
 async def update_subscription_plan(
