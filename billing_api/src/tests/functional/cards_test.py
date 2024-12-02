0aa9680b9df375280260c309ac15f9ff6572d10b
Vlad <belouskovlad@gmail.com>
2024-12-02 10:47:10 +0600

    добавил тест на запрос инициализации создания карты

--- billing_api/src/tests/functional/cards_test.py
+++ billing_api/src/tests/functional/cards_test.py
@@ -1,8 +1,11 @@
 from http import HTTPStatus
+from unittest.mock import AsyncMock, patch
 
 import pytest
+from sqlalchemy import select
 
 from models.enums import StatusCardsEnum
+from models.models import UserCardsStripe
 
 
 class TestGetUserCards:
@@ -64,3 +67,33 @@ async def test_get_success_card(api_client) -> None:
     response = await api_client.get(form_path)
     assert response.status_code == HTTPStatus.OK
     assert response.headers["Content-Type"] == "text/html; charset=utf-8"
+
+
+class TestInitializePaymentMethod:
+    def setup_method(self):
+        self.path = "/api/v1/billing/create-checkout-session/"
+
+    @pytest.mark.asyncio(loop_scope="session")
+    @patch("services.payment_process.PaymentProcessorStripe.create_card", new_callable=AsyncMock)
+    async def test_initialize_payment_method(
+        self, mock_create_card, api_client, access_token_user, test_session
+    ) -> None:
+        """Проверка успешного запроса на создание карты."""
+        mock_create_card.return_value = "https://mocked.stripe.url/checkout"
+
+        response = await api_client.post(self.path, headers=access_token_user)
+
+        result = await test_session.execute(select(UserCardsStripe))
+        cards = result.scalars().all()
+
+        assert response.status_code == 303
+        assert response.headers["location"] == "https://mocked.stripe.url/checkout"
+        assert len(cards) == 1
+
+    @pytest.mark.asyncio(loop_scope="session")
+    async def test_initialize_payment_method_for_anonymous_user(self, api_client) -> None:
+        """Проверка на анонимного юзера."""
+        response = await api_client.post(self.path)
+
+        assert response.status_code == HTTPStatus.FORBIDDEN
+        assert response.json() == {"detail": "Not authenticated"}
