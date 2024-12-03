aee5230d2aa2a54ba76b5da7c884757d24103b01
Natalia B <naffa.bob@gmail.com>
2024-12-03 17:40:12 +0500

    Добавлены методы на отмену, создание для админа

--- billing_api/src/api/v1/subscription.py
+++ billing_api/src/api/v1/subscription.py
@@ -4,7 +4,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, Path
 from fastapi_pagination import Page, paginate
 
-from api.jwt_access_token import AccessTokenPayload, UserRole, security_jwt
+from api.jwt_access_token import AccessTokenPayload, UserRole, require_admin, security_jwt
 from api.utils import generate_error_responses, subscription_query_params
 from schemas.subscription import SubscriptionCreate, SubscriptionRenew, SubscriptionResponse
 from services.subscription_manager import SubscriptionManager, get_subscription_manager
@@ -13,7 +13,7 @@ router = APIRouter()
 
 
 @router.get(
-    "/{subscription_id}",
+    "/admin/{subscription_id}",
     response_model=SubscriptionResponse,
     summary="Вывести подписку",
     description="Вывести параметры подписки по id",
@@ -21,6 +21,8 @@ router = APIRouter()
     responses=generate_error_responses(
         HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.NOT_FOUND, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED
     ),  # type: ignore[reportArgumentType]
+    dependencies=[Depends(require_admin)],
+    tags=["Admin"],
 )
 async def get_subscription_by_id(
     subscription_id: UUID = Path(..., description="ID подписки"),
@@ -30,13 +32,32 @@ async def get_subscription_by_id(
 
 
 @router.get(
+    "/{subscription_id}",
+    response_model=SubscriptionResponse,
+    summary="Вывести подписку пользователя",
+    description="Вывести параметры подписки пользователя по id",
+    status_code=HTTPStatus.OK,
+    responses=generate_error_responses(
+        HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.NOT_FOUND, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED
+    ),  # type: ignore[reportArgumentType]
+)
+async def get_user_subscription_by_id(
+    subscription_id: UUID = Path(..., description="ID подписки"),
+    subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
+    token: AccessTokenPayload = Depends(security_jwt),
+):
+    return await subscription_manager.get_user_subscription(token.user_id, subscription_id)
+
+
+@router.get(
     "/",
     response_model=Page[SubscriptionResponse],
-    summary="Вывести подписки",
-    description="Вывести все существующие подписки с пагинацией",
+    summary="Вывести все подписки",
+    description="Админ может просмотреть все подписки, пользователь только свои.",
     status_code=HTTPStatus.OK,
     responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.UNAUTHORIZED),
     # type: ignore[reportArgumentType]
+    tags=["Admin"],
 )
 async def get_subscriptions(
     subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
@@ -68,6 +89,24 @@ async def create_subscription(
 
 
 @router.post(
+    "/admin",
+    response_model=SubscriptionResponse,
+    summary="Создать подписку для пользователя",
+    description="Создаёт подписку для пользователя",
+    status_code=HTTPStatus.CREATED,
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.BAD_REQUEST, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
+    dependencies=[Depends(require_admin)],
+    tags=["Admin"],
+)
+async def create_user_subscription(
+    user_id: UUID,
+    subscription_data: SubscriptionCreate,
+    subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
+):
+    return await subscription_manager.create_subscription(user_id, subscription_data)
+
+
+@router.post(
     "/{subscription_id}/cancel",
     response_model=SubscriptionResponse,
     summary="Отменить подписку",
@@ -81,6 +120,24 @@ async def cancel_subscription(
     token: AccessTokenPayload = Depends(security_jwt),
 ):
     return await subscription_manager.cancel_subscription(user_id=token.user_id, subscription_id=subscription_id)
+
+
+@router.post(
+    "admin/{subscription_id}/cancel",
+    response_model=SubscriptionResponse,
+    summary="Отменить подписку для пользователя",
+    description="Отменяет выбранную подписку для пользователя",
+    status_code=HTTPStatus.OK,
+    responses=generate_error_responses(HTTPStatus.INTERNAL_SERVER_ERROR, HTTPStatus.FORBIDDEN, HTTPStatus.NOT_FOUND),  # type: ignore[reportArgumentType]
+    dependencies=[Depends(require_admin)],
+    tags=["Admin"],
+)
+async def cancel_user_subscription(
+    user_id: UUID,
+    subscription_id: UUID = Path(..., description="ID подписки"),
+    subscription_manager: SubscriptionManager = Depends(get_subscription_manager),
+):
+    return await subscription_manager.cancel_subscription(user_id=user_id, subscription_id=subscription_id)
 
 
 @router.post(
