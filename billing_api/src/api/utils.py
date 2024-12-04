6a7e1ce719217909cca54bc17e7a6dfc2e6e52bd
Ivan Skvortsov <pprofcheg@gmail.com>
2024-12-04 16:12:20 +0300

    Добавлена фильтрация по user_id для выгрузки подписок админом

--- billing_api/src/api/utils.py
+++ billing_api/src/api/utils.py
@@ -27,15 +27,25 @@ def transaction_query_params(
     subscription_id: UUID | None = None,
     status: Annotated[TransactionStatus | None, Query()] = None,
     payment_type: Annotated[PaymentType | None, Query()] = None,
-) -> dict[str, TransactionStatus | PaymentType]:
-    query_params = {"subscription_id": subscription_id, "status": status, "payment_type": payment_type}
+) -> dict[str, Any]:
+    query_params = {
+        "subscription_id": subscription_id,
+        "status": status,
+        "payment_type": payment_type,
+    }
     return {k: v for k, v in query_params.items() if v is not None}
 
 
 def subscription_query_params(
     status: Annotated[SubscriptionStatus | None, Query()] = None,
+    user_id: Annotated[UUID | None, Query()] = None,
     plan_id: Annotated[UUID | None, Query()] = None,
     auto_renewal: Annotated[bool | None, Query()] = None,
-) -> dict[str, TransactionStatus | PaymentType]:
-    query_params = {"status": status, "plan_id": plan_id, "auto_renewal": auto_renewal}
+) -> dict[str, Any]:
+    query_params = {
+        "status": status,
+        "plan_id": plan_id,
+        "user_id": user_id,
+        "auto_renewal": auto_renewal,
+    }
     return {k: v for k, v in query_params.items() if v is not None}
